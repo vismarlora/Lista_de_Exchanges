09d321228889cc3a0cd28a1343a1707e9fc9e0e7
vismarlora <vismar_lora@ucne.edu.do>
2022-06-22 00:37:38 -0400

    Heredando de HiltViewModel en el MainActivity.kt

--- app/src/main/java/com/example/listadeexchanges/MainActivity.kt
+++ app/src/main/java/com/example/listadeexchanges/MainActivity.kt
@@ -30,6 +30,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import retrofit2.HttpException
 import retrofit2.http.GET
@@ -170,3 +171,29 @@ data class ExchangeListState(
     val exchanges: List<ExchangeDto> = emptyList(),
     val error: String = ""
 )
+
+@HiltViewModel
+class ExchangeViewModel @Inject constructor(
+    private val exchangesRepository: ExchangesRepository
+) : ViewModel() {
+
+    private var _state = mutableStateOf(ExchangeListState())
+    val state: State<ExchangeListState> = _state
+
+    init {
+        exchangesRepository.getExchanges().onEach { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    _state.value = ExchangeListState(isLoading = true)
+                }
+
+                is Resource.Success -> {
+                    _state.value = ExchangeListState(exchanges = result.data ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _state.value = ExchangeListState(error = result.message ?: "Error desconocido")
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
